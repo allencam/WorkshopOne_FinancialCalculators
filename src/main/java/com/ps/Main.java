e3fcaf192a66441cd7908f69fe60a752f9ffbca5
Camron Allen <allencam093@gmail.com>
2024-09-27 15:34:05 -0500

    added selection prompt and prompts for user input to get variables for mortgageCalc

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -10,12 +10,30 @@ public class Main {
         print calculated values
         end
      */
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Welcome, what would you like to do?");
+        System.out.println("(a)  Mortgage Calculation");
+        System.out.println("(b)  CD Future Value Calculator");
+        System.out.print("Enter your selection: ");
+        String select = scan.nextLine();
+
+        if(select.equals("a")) {
+            System.out.print("Enter the principal loan amount: ");
+            float principal = scan.nextFloat();
+            System.out.print("Enter the interest rate without the % sign: ");
+            float rate = scan.nextFloat();
+            System.out.print("Enter the length of the loan in months: ");
+        } else if(select.equals("b")) {
+            System.out.println("This feature is currently in development.");
+        } else {
+            System.out.println("Invalid entry.");
+        }
 
     }
 }
 
-public static float mortgageCalc() {
+public static float mortgageCalc(float principal,float rate,short loanLengthInMonths) {
 /* ===== Mortgage Calc =====
     formula: M (monthly principal and interest payment) = P (principal) * (i [monthly interest rate] / (1 - (1 + i) ^ L (length of loan in months)
     important notes: % values are divided by 100, %.2f will round an interpolated floating point value to two decimal places
