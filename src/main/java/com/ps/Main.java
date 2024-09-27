bec2f6129d67afe90b51fb21ec42b43416c4adb2
Camron Allen <allencam093@gmail.com>
2024-09-27 14:59:18 -0500

    laid out a plan for main method, created mortgageCalc method and plan for it, imported Scanner class

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,24 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    /* ===== Main Method ======
+        start
+        ask user if they want (a) - mortgage calc, or (b) CD future value calc ( else/if else )
+        ask user to provide values based on their selection and run respective methods depending on condition, create variables here
+        print calculated values
+        end
+     */
+
 
     }
+}
+
+public static float mortgageCalc() {
+/* ===== Mortgage Calc =====
+    formula: M (monthly principal and interest payment) = P (principal) * (i [monthly interest rate] / (1 - (1 + i) ^ L (length of loan in months)
+    important notes: % values are divided by 100, %.2f will round an interpolated floating point value to two decimal places
+ */
+
 }
