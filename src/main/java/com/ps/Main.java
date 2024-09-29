3cb08ae377b6c544aba923fa166e7b962ff8d127
Camron Allen <allencam093@gmail.com>
2024-09-28 20:05:24 -0500

    for cdCalc, finished user input under Main, added formula for calculating future value of CD and return value

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -31,25 +31,32 @@ public class Main {
             System.out.printf("Monthly payment: $%.2f", monthlyPayment);
         } else if(select.equals("b")) {
             // Get necessary variables
+            System.out.print("Enter the initial deposit amount: ");
+            double principal = scan.nextDouble();
+            System.out.print("Enter the annual interest rate without the % sign: ");
+            float annualInterestRate = scan.nextFloat();
+            System.out.print("How many times does interest compound per year (most CDs compound daily, so 365 times): ");
+            short compFrequency = scan.nextShort();
+            System.out.print("How many years until the maturity date? ");
+            byte timeInYears = scan.nextByte();
+            double futureValue = cdCalc(principal,annualInterestRate,compFrequency,timeInYears);
+            System.out.printf("Future Value: $%.2f",futureValue);
         } else {
             System.out.println("Invalid entry.");
         }
     }
-
     public static double mortgageCalc(float principal,float monthlyRate,short loanLengthInYears) {
 //      ===== Mortgage Calc =====
 //      - formula: M (monthly principal and interest payment) = P (principal) * (i [monthly interest rate] / (1 - (1 + i) ^ L (length of loan in months)
 //      - important notes: % values are divided by 100, %.2f will round an interpolated floating point value to two decimal places
-
         double monthlyPrincipal = principal * ((monthlyRate/12/100) / (1 - (Math.pow(1 + (monthlyRate/12/100),(-loanLengthInYears*12)))));
         return monthlyPrincipal;
     }
-    public static double cdCalc() {
+    public static double cdCalc(double principal,float annualRate,short compFrequency,byte timeInYears) {
 //      ===== CD Future Value Calc =====
-//      - formula: A (futureValue) = P (initial amount, principal) * (1 + R [annualInterestRate] / n (number of times interest is compounded per year, compFrequency)
-//      - Variables/Data Types: futureValue/double, principal/double, annualRate/float, compFrequency/short
-        return 0;
-
-
+//      - formula: A (futureValue) = P (initial amount, principal) * (1 + R [annualInterestRate] / n (number of times interest is compounded per year, compFrequency) ^ (n * t (time in years))
+//      - Variables/Data Types: futureValue/double (return), principal/double, annualRate/float, compFrequency/short, timeInYears/byte
+        double futureValue = principal * (Math.pow((1 + (annualRate/100) / compFrequency),(compFrequency * timeInYears)));
+        return futureValue;
     }
 }
